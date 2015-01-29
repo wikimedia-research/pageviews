64e55ebaefda86d89ce6c1fbb103f07f9afc227e
ironholds <ironholds@gmail.com>
2015-01-29 00:58:37 -0500

    add RCXX framework

---
+++ R/pageviews.R
@@ -0,0 +1,10 @@
+#' @title Functions for handling pageviews data
+#' @name pageviews
+#' @description This package provides functions for handling the Wikimedia request logs
+#' and performing a variety of operations on them, most prominently extracting "pageviews"
+#' and classifying them.
+#' @useDynLib pageviews
+#' @importFrom Rcpp sourceCpp
+#' @docType package
+#' @aliases pageviews pageviews-package
+NULL
